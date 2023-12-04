8ec114a930a538114168c11fc97233b243ce114b
unknown <cesar.ruiz009@mymdc.net>
2023-12-04 10:59:31 -0500

    code

---
+++ Assets/ShotManager.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Shot
+{
+    public float upForce;
+    public float hitForce;
+}
+public class ShotManager : MonoBehaviour
+{
+    public Shot topSpin;
+    public Shot flat;
+}
