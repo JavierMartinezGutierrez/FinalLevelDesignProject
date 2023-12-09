23a3cc69353079d88370cbd40fe786437b5c1700
terrellturner1523/Four <terrell.turner001@mymdc.net>
2023-12-09 15:23:29 -0500

    Menu

---
+++ Assets/MainMenu2.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu2: MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("DodgeBall");
+    }
+    public void Options()
+    {
+        SceneManager.LoadScene("Options");
+    }
+    public void QuitGame()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+    public void PauseGame()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+}
+
